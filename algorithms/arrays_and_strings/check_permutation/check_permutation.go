dfd5a705278fdd1d9b067b0653f17105000c0963
aviola <ing.viola.alessio@gmail.com>
2021-08-11 12:22:17 +0200

    Add check_permutation algorithm

---
+++ algorithms/arrays_and_strings/check_permutation/check_permutation.go
@@ -0,0 +1,31 @@
+package check_permutation
+
+// Assumption: ASCII
+func CheckPermutation(str1, str2 string) bool {
+	strLen := len(str1)
+
+	if strLen != len(str2) {
+		return false
+	}
+
+	if strLen == 0 {
+		return true
+	}
+
+	var letters [128]int
+	for i := 0; i < strLen; i++ {
+		asciiCode := int(str1[i])
+		letters[asciiCode]++
+	}
+
+	for i := 0; i < strLen; i++ {
+		asciiCode := int(str2[i])
+		if letters[asciiCode] == 0 {
+			return false
+		} else {
+			letters[asciiCode]--
+		}
+	}
+
+	return true
+}
